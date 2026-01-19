919ccb097c3a367cf7bc63c848d64767998b1261
Amoiscool <amobat72@gmail.com>
2026-01-19 23:03:28 +0000

    cool

---
+++ main.js
@@ -0,0 +1,15 @@
+const button = document.querySelector(".button")
+
+const para = document.querySelector(".para")
+   
+
+
+
+     function functionName() {
+para.classList.add("newClass")
+console.log("it worked :)")
+    } if {
+        
+    }
+
+    
